dc4f3c24b8e7efc3412b733a3b7926685060954b
Lena <eutereshkova@mail.ru>
2022-09-19 11:13:27 +0300

    добавили задачу 34

---
+++ задача34/Program.cs
@@ -0,0 +1,12 @@
+﻿int size = 4;
+int [] array = new int [size];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100);//[0,999]
+    Console.Write(array[i] + "\t");
+    if (array[i] %2 == 0)
+    count++;
+   
+}
+Console.WriteLine($"{"\n"} количество четных чисел: {count}");
